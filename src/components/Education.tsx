c9024707b245a3429b9624b38b077215624c3ef4
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-02-25 01:05:07 +0530

    education fix

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 // import me from "../assets/img/me.png";
 import { educationData } from "../assets/lib/data";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay, Pagination, Navigation } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -11,8 +11,8 @@ import { useSectionInView } from "../assets/lib/hooks";
 import { useLanguage } from "../context/language-context";
 
 const Education: React.FC = () => {
-  const progressCircle = useRef<SVGSVGElement | null>(null);
-  const progressContent = useRef<HTMLSpanElement | null>(null);
+  // const progressCircle = useRef<SVGSVGElement | null>(null);
+  // const progressContent = useRef<HTMLSpanElement | null>(null);
   const { ref } = useSectionInView("Education");
   const { language } = useLanguage();
   const animationReference = useRef<HTMLDivElement>(null);
@@ -22,15 +22,15 @@ const Education: React.FC = () => {
   });
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
-  const onAutoplayTimeLeft = (_s: unknown, time: number, progress: number) => {
-    if (progressCircle.current && progressContent.current) {
-      progressCircle.current.style.setProperty(
-        "--progress",
-        String(1 - progress)
-      );
-      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-    }
-  };
+  // const onAutoplayTimeLeft = (_s: unknown, time: number, progress: number) => {
+  //   if (progressCircle.current && progressContent.current) {
+  //     progressCircle.current.style.setProperty(
+  //       "--progress",
+  //       String(1 - progress)
+  //     );
+  //     progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  //   }
+  // };
   const paragraphs =
     language === "DE"
       ? educationData.paragraphs_DE
@@ -38,37 +38,102 @@ const Education: React.FC = () => {
 
   return (
     <React.Fragment>
-      <sec className="bg-gray-100 py-8">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-gray-800 mb-8">Education</h2>
-          <div className="relative wrap overflow-hidden p-10 h-full">
-            {/* Timeline */}
-            <div
-              className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
-              style={{ left: "50%" }}
-            ></div>
-            {/* Education items */}
-            <div className="mb-8 flex justify-between items-center w-full right-timeline">
-              <div className="order-1 w-5/12"></div>
-              <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 className="mx-auto text-white font-semibold text-lg">1</h1>
+      <section className="education relative mt-16 " id="education" ref={ref}>
+        <div className="title-container flex flex-col gap-6 justify-center items-center p-32 w-1/2 max-lg:w-full max-lg:p-16 max-lg:items-start">
+          <motion.div
+            ref={animationReference}
+            style={{
+              scale: scaleProgess,
+              opacity: opacityProgess,
+              textAlign: "left",
+            }}
+          >
+            <p className="text-[--black] mb-6">
+              <span className="text-orange">&lt;</span>
+              {language === "DE" ? educationData.title : educationData.title_EN}
+              <span className="text-orange">/&gt;</span>
+            </p>
+
+            <h2 className="text-[--black] text-center max-lg:text-left break-words">
+              {language === "DE"
+                ? educationData.description
+                : educationData.description_EN}
+            </h2>
+          </motion.div>
+        </div>
+        <div className="flex flex-row justify-center gap-6 items-center pl-32 pr-32 mb-16  max-lg:flex-col max-lg:p-16 min-[1921px]:px-[45rem] min-[1921px]:mb-48">
+          {/* <article className="pl-60 max-lg:p-0">
+            <img src={me} alt={me} />
+          </article> */}
+          <div
+            // spaceBetween={100}
+            // centeredSlides={true}
+            // autoplay={{
+            //   delay: 10000,
+            //   disableOnInteraction: false,
+            // }}
+            // pagination={{
+            //   clickable: true,
+            // }}
+            // modules={[Autoplay, Pagination, Navigation]}
+            // onAutoplayTimeLeft={onAutoplayTimeLeft}
+            className="w-1/2 pt-32 relative z-2 pr-20 pb-32 pl-20 max-lg:w-full max-lg:pt-16 max-lg:pb-16 max-lg:pl-0 max-lg:pr-0 min-[1921px]:w-1/2
+            bg-[--darkblue] text-[--white] flex flex-col justify-center items-start gap-24 rounded-2xl p-20 border-solid border-[0.4rem] border-[--lightblue] hover:border-orange duration-500 transition-all text-left max-lg:p-10 cursor-grab"
+          >
+            {paragraphs.map((paragraph, index) => (
+              <div
+                className="bg-[--darkblue] text-[--white] flex flex-col justify-center items-start gap-24 rounded-2xl p-20 border-solid border-[0.4rem] border-[--lightblue] hover:border-orange duration-500 transition-all text-left max-lg:p-10 cursor-grab"
+                key={index}
+              >
+                <div className="flex gap-6 flex-row justify-start items-center max-lg:flex-col max-lg:justify-center max-lg:text-center ">
+                  <div>
+                    <img
+                      src={paragraph.icon}
+                      alt={paragraph.icon}
+                      className="w-24"
+                    />
+                  </div>
+                  <div>
+                    <h2>{paragraph.degree}</h2>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-10 max-lg:flex-col">
+                  <div className="flex flex-col gap-4 items-center justify-between -mt-10 -mb-10 max-lg:flex-row max-lg:m-0">
+                    <p className="text-white">
+                      <span className="text-orange">&lt;</span>
+                      {paragraph.end}
+                      <span className="text-orange">/&gt;</span>
+                    </p>
+                    <div className="flex justify-between items-center w-1 h-[100%] max-lg:flex-row max-lg:w-[10rem]  max-lg:bg-lightblue">
+                      <div></div>
+                      <div className="w-[0.5rem] bg-[--lightblue] h-[100%] max-lg:w-10  max-lg:bg-lightblue max-lg:h-[0.25rem]"></div>
+                      <div></div>
+                    </div>
+                    <p className="text-white">
+                      <span className="text-orange">&lt;</span>
+                      {paragraph.start}
+                      <span className="text-orange">/&gt;</span>
+                    </p>
+                  </div>
+                  <div>
+                    <span className="text-orange text-4xl">{paragraph.degree}</span>
+                    <p className="text-white text-2xl mt-2">{paragraph.institute}</p>
+                  </div>
+                </div>
               </div>
-              <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 className="mb-3 font-bold text-gray-800 text-xl">
-                  Bachelor of Technology
-                </h3>
-                <p className="text-sm leading-snug tracking-wide text-gray-900">
-                  ABES Engineering College
-                </p>
-                <p className="text-xs leading-snug tracking-wide text-gray-900">
-                  2018 - 2022
-                </p>
-              </div>
-            </div>
-            {/* Add more education items here */}
+            ))}
+            {/* <div
+              className="autoplay-progress absolute right-0 bottom-0 z-10 flex items-center justify-center font-bold text-orange text-4xl w-24 h-24 max-lg:w-16 max-lg:h-16 max-lg:text-3xl "
+              slot="container-end"
+            >
+              <svg viewBox="0 0 48 48" ref={progressCircle}>
+                <circle cx="24" cy="24" r="20"></circle>
+              </svg>
+              <span ref={progressContent}></span>
+            </div> */}
           </div>
         </div>
-      </sec>
+      </section>
     </React.Fragment>
   );
 };
